4a19ee75e280026a8cbfa35326834d91d900799b
mishka832 <mishkaraheja30@gmail.com>
2025-08-10 11:04:53 +0530

    Updated frontend pages and added backend folder

---
+++ backend/config/userdb.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const connectDb=async()=>{
+    try {
+        const dbConnection=await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
+        console.log(`Database Connected: ${dbConnection.connection.host}`);
+    
+    }catch(error) {
+        console.log(`Database Could not be connected ${error.message}`);
+    }
+}
+module.exports=connectDb;
--- pages/Addrecords.jsx
+++ pages/Addrecords.jsx
@@ -1,207 +1,203 @@
-import React from 'react'
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import Buttons from '../components/Button';
-import Alert from 'react-bootstrap/Alert';
+import React, { useState } from "react";
+import Buttons from "../components/Button";
+import { useNavigate } from "react-router-dom";
 const Addrecords = () => {
+  const navigate = useNavigate();
+  const [date, setDate] = useState("");
+  const [doctor, setDoctor] = useState("");
+  const [hospital, setHospital] = useState("");
+  const [files, setFiles] = useState([]);
+  const [bodyOrgan, setBodyOrgan] = useState("");
+  const [medicine, setMedicine] = useState("");
+  const [title, setTitle] = useState("");
+  const [notes, setNotes] = useState("");
 
-    const [date,setDate] = useState('');
-    const[doctor,setDoctor] = useState("");
-    const[hospital,setHospital] = useState("");
-    const [files,setFiles] = useState([]);
-    const [bodyOrgan,setBodyOrgan] = useState("");
-    const [medicine,setMedicine] = useState("");
-    const[title,setTitle] = useState("");
-    const[notes,setNotes] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const onSubmitHandler= async (e)=>{
-        e.preventDefault();
-        if(!date || !title.trim()){
-            alert("Please enter date and title ")
-            return;
-        }
-        const formData = new FormData()
-            formData.append('date',date);
-            formData.append('title',title);
-            formData.append('doctor',doctor);
-            formData.append('hospital',hospital);
-            formData.append('medicine',medicine);
-            formData.append('notes',notes);
-            formData.append('bodyOrgan',bodyOrgan);
-           
-            files.forEach(({file}) =>{
-                formData.append('files',file);
-            });
+    const formData = new FormData();
+    formData.append("date", date);
+    formData.append("doctor", doctor);
+    formData.append("hospital", hospital);
+    formData.append("bodyOrgan", bodyOrgan);
+    formData.append("medicine", medicine);
+    formData.append("title", title);
+    formData.append("notes", notes);
 
-            for(const [key,value] of formData.entries()){
-                console.log(key,value)
-            }
+    files.forEach((file) => {
+      formData.append("files", file); // directly append the File object
+    });
 
-            // try{
-            //     const response = await fetch('http://localhost:3000/',{
-            //         method:'POST',
-            //         body:formData
-            //     })
-            //     if(!response.ok){
-            //         throw new Error('Failed to Submit record');
-            //     }
-            //     const result = await response.json();
-            //     console.log('success',result);
-            //     alert('record submitted successfully');
-            //     setDate('');
-            //     setTitle('');
-            //     setDoctor('');
-            //     setHospital('');
-            //     setMedicine('');
-            //     setNotes('');
-            //     setBodyOrgan('');
-            //     setFiles([]);
-            // }
-            // catch(error){
-            //     console.log(error.message)
-            //     alert(`error:${error.message}`)
-            // }
+    try {
+      const res = await fetch("http://localhost:3000/api/user/add-details", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: formData,
+      });
 
-
+      const data = await res.json();
+      console.log(data);
+      navigate("/home");
+    } catch (err) {
+      console.error(err);
     }
-    const removeFile =(id)=>{
-        setFiles(prev=>prev.filter(f=>f.id !== id))
-    }
-    const fileChangeHandler=(e)=>{
-        const selectedFiles = Array.from(e.target.files).map(file=>({file,id:file.name+"_"+file.lastModified}))
-        setFiles(prev =>[...prev,...selectedFiles])
-    }
+  };
+
+  const removeFile = (index) => {
+    setFiles((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const fileChangeHandler = (e) => {
+    setFiles((prev) => [...prev, ...Array.from(e.target.files)]);
+  };
+
   return (
-    <div style={{maxWidth:'900px', margin:'auto',padding:'20px', backgroundColor:'#87b5c4ff'}}>
+    <div
+      style={{
+        maxWidth: "900px",
+        margin: "auto",
+        padding: "20px",
+        backgroundColor: "#87b5c4ff",
+      }}
+    >
       <h2>Add Medical Record</h2>
-      <p>{date}{title}</p>
-      <form onSubmit={onSubmitHandler}>
-        
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Enter Date:</label>
-            <div className='col-sm-8 '>
-                <input className="form-control"type="date" onChange={(e)=>{setDate(e.target.value)}}/>
-            </div>
+      <form onSubmit={handleSubmit}>
+        {/* Date */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Enter Date:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Record Title:</label>
-            <div className='col-sm-8'>
-                <input 
-                className="form-control" 
-                type="text" 
-                placeholder="Enter record-title"
-                value={title}
-                onChange={(e)=>{
-                    setTitle(e.target.value)
-                }}/>
-            </div>
+        {/* Title */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Record Title:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter record title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Doctor Name:</label>
-            <div className='col-sm-8'>
-                <input 
-                className="form-control" 
-                type="text" 
-                placeholder="Enter Doctor Name"
-                value={doctor}
-                onChange={(e)=>{
-                    setDoctor(e.target.value)
-                }}/>
-            </div>
+        {/* Doctor */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Doctor Name:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter Doctor Name"
+              value={doctor}
+              onChange={(e) => setDoctor(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Hospital Name:</label>
-            <div className='col-sm-8'>
-                <input 
-                className="form-control" 
-                type="text" 
-                placeholder="Enter Hospital Name"
-                value={hospital}
-                onChange={(e)=>{
-                    setHospital(e.target.value)
-                }}/>
-            </div>
+        {/* Hospital */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Hospital Name:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter Hospital Name"
+              value={hospital}
+              onChange={(e) => setHospital(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Body Organ:</label>
-            <div className='col-sm-8'>
-                <input 
-                className="form-control" 
-                type="text" 
-                placeholder="Enter Body Organ"
-                value={bodyOrgan}
-                onChange={(e)=>{
-                    setBodyOrgan(e.target.value)
-                }}/>
-            </div>
+        {/* Body Organ */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Body Organ:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter Body Organ"
+              value={bodyOrgan}
+              onChange={(e) => setBodyOrgan(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Medicines:</label>
-            <div className='col-sm-8'>
-                <input 
-                className="form-control" 
-                type="text" 
-                placeholder="Enter medicines"
-                value={medicine}
-                onChange={(e)=>{
-                    setMedicine(e.target.value)
-                }}/>
-            </div>
+        {/* Medicine */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Medicines:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter medicines"
+              value={medicine}
+              onChange={(e) => setMedicine(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Notes:</label>
-            <div className='col-sm-8'>
-                <input 
-                className="form-control" 
-                type="text" 
-                placeholder="Enter any other information"
-                value={notes}
-                onChange={(e)=>{
-                    setNotes(e.target.value)
-                }}/>
-            </div>
+        {/* Notes */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Notes:</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter any other information"
+              value={notes}
+              onChange={(e) => setNotes(e.target.value)}
+            />
+          </div>
         </div>
-        
-        <div className='row mb-3'>
-            <label className='col-sm-4 col-form-label'>Add Files:</label>
-            <div className='col-sm-8'>
-                <input 
-                multiple
-                accept="image/*,application/pdf"
-                className="form-control" 
-                type="file" 
-                placeholder="Enter record-title"
-                onChange={fileChangeHandler}/>
 
-                {files.length > 0 && (
-                    <ul className='list-group mt-2'>
-                        {files.map(({file,id},index)=>(
-                            <li key={id}
-                            className='list-group-item d-flex justify-content-between align-items-center'>
-                            {file.name}
-                                <button type="button"className='btn btn-sm btn-outline-danger'
-                                onClick={()=>{removeFile(id)}}>
-                                    &times;
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
+        {/* Files */}
+        <div className="row mb-3">
+          <label className="col-sm-4 col-form-label">Add Files:</label>
+          <div className="col-sm-8">
+            <input
+              multiple
+              accept="image/*,application/pdf"
+              className="form-control"
+              type="file"
+              onChange={fileChangeHandler}
+            />
+            {files.length > 0 && (
+              <ul className="list-group mt-2">
+                {files.map((file, index) => (
+                  <li
+                    key={index}
+                    className="list-group-item d-flex justify-content-between align-items-center"
+                  >
+                    {file.name}
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-danger"
+                      onClick={() => removeFile(index)}
+                    >
+                      &times;
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
-            <Buttons 
-            Bcontent="Submit"
-            handleClick={onSubmitHandler}/>
 
-      </form >
+        <Buttons Bcontent="Submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Addrecords
+export default Addrecords;
